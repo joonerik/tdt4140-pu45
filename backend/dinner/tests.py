841b56de381c2e186eb8015008ed9725547fbd58
tord-s <tord@softeland.name>
2021-02-09 16:45:24 +0100

    Add: test file with simple test

--- backend/dinner/tests.py
+++ backend/dinner/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.utils import timezone
+
+from .models import Dinner
+import datetime
+
+
+class DinnerTestCase(TestCase):
+    def setUp(self):
+        Dinner.objects.create(title="test_object", capacity="100")
+        Dinner.objects.create(title="test_object_2", capacity="1")
+
+    def test_dinner_creation(self):
+        test_object_2 = Dinner.objects.get(capacity=1)
+        self.assertEqual(test_object_2.title, 'test_object_2')
+        self.assertEqual(test_object_2.beskrivelse, 'ikke oppgitt')
+        self.assertEqual(test_object_2.date_posted.day, datetime.date.today().day)
