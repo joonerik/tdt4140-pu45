add3be8de5075ebb280e1b7fda506237fcc07867
Henrik Viken Lied <henrikvl@dhcp-10-24-0-190.wlan.ntnu.no>
2021-02-11 15:52:12 +0100

    feat: add stylesheet for DinnerForm and adjust styling

--- frontend/src/pages/DinnerForm.js
+++ frontend/src/pages/DinnerForm.js
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import "./style/DinnerForm.css"
 
 export default function AddressForm() {
   return (
