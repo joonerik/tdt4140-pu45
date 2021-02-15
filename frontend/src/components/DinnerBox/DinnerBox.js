229a98f36e3a4fca38d1702e70e9f5e542a715bb
Joon-Erik Sæther <joon-erik@hotmail.com>
2021-02-15 16:10:23 +0100

    style: add styling in DinnerBox component

--- frontend/src/components/DinnerBox/DinnerBox.js
+++ frontend/src/components/DinnerBox/DinnerBox.js
@@ -4,39 +4,42 @@ import "./DinnerBox.css"
 function DinnerBox(props) {
 
     const [content, setContent] = useState(0);
+    const color = "lightblue"
     var info = null;
 
     if (content === 1) {
-        info = <div>
+        info = <div className="dinnerDetails">
             {props.card.location}
         </div>
     } else if (content === 2) {
-        info = <p>fuck</p>
+        info = <div className="dinnerDetails">Participants</div>
     } else if (content === 3) {
-        info = <p>f</p>
+        info = <div className="dinnerDetails">Allergies</div>
     }
 
 
     return (
         <div className="box">
             <button onClick={props.state}>X</button>
-            <h1>{props.card.title.toUpperCase()}</h1>
-            <p>Ola Nordmann, 90129028</p>
-            <p>Bakklandet</p>
-            <p>01.01.21 kl: 1900</p>
+            <ul className="dinnerInfo">
+                <h1>{props.card.title.toUpperCase()}</h1>
+                <p>Ola Nordmann, 90129028</p>
+                <p>Bakklandet</p>
+                <p>01.01.21 kl: 1900</p>
+            </ul>
             {/* <p>Location: {props.card.location}</p>
             <p>Host: {props.card.host}</p>
             <p>Capacity: {props.card.capacity}</p> */}
             <ul id="navigation">
                 <li onClick={() => {
                     setContent(1)
-                }}>Description</li>
+                }} style={content === 1 ? {color} : null}>Description</li>
                 <li onClick={() => {
                     setContent(2)
-                }}>Participants</li>
+                }} style={content === 2 ? {color} : null}>Participants</li>
                 <li onClick={() => {
                     setContent(3)
-                }}>Allergies</li>
+                }} style={content === 3 ? {color} : null} >Allergies</li>
             </ul>  
             {info}
         
