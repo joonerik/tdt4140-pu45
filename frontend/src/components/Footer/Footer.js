0127abd423aceaf97417a9cfb11f4b07794a6610
Joon-Erik Sæther <joon-erik@hotmail.com>
2021-02-15 16:13:19 +0100

    style: move footer to bottom of page, probably not working if scrolling enabled

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -19,6 +19,9 @@ const useStyles = makeStyles((theme) => ({
   footer: {
     backgroundColor: "theme.palette.background.paper",
     padding: theme.spacing(6),
+    position: "absolute",
+    bottom: "0",
+    width: "100%",
   },
 }));
 
