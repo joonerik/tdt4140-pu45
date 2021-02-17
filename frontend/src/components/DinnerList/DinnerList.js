075ddd1970554ad94b9391a93ac5a8e54d61db76
Joon-Erik Sæther <joon-erik@hotmail.com>
2021-02-17 12:52:50 +0100

    refactor: try to change DinnerList to functional comp, not working yet

--- frontend/src/components/DinnerList/DinnerList.js
+++ frontend/src/components/DinnerList/DinnerList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -17,69 +17,82 @@ function convert(date) {
 
 }
 
-export default class DinnerList extends React.Component {
-  state = {
-    events: [],
-    isShowing : false,
-    id : null,
-  };
+function DinnerList {
+  // state = {
+  //   events: [],
+  //   isShowing : false,
+  //   id : null,
+  // };
+
+  const [events, setEvents] = useState([]);
+  const [showing, setShowing] = useState(false);
+  const [id, setId] = useState(null);
+
+  // modifyState = () =>{
+  //   this.setState({isShowing: !this.state.isShowing})   
+  // }
+
+  // componentDidMount() {
+  //   axios.get("https://dinnerpool.herokuapp.com/dinners/").then((res) => {
+  //     this.setState({ events: res.data });
+  //   });
+  // }
+  useEffect(() => {
+    axios.get("https://dinnerpool.herokuapp.com/dinners/").then((res) => {
+      // this.setState({ events: res.data });
+      setEvents(res.data)
+    });
+  }, [])
   
-  modifyState = () =>{
-    this.setState({isShowing: !this.state.isShowing})   
+  return (
+    <div>
+      <Container maxWidth="md">
+        {/* End hero unit */}
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Link to="/login">
+                <Card style={{ height: '100%' }}>
+                  <CardContent>
+                      <AddIcon style={{ color: "green", fontSize:"100px", paddingTop: "15px" }} ></AddIcon>
+                  </CardContent>
+                </Card>
+              </Link>
+            </Grid>
+          {events.length > 0 &&
+            // use event id as key instead
+            events.map((card, i) => (
+              <Grid item key={card.id} xs={12} sm={6} md={4}>
+                <Card>
+                  <CardContent>
+                    <Typography variant="subtitle2" align="left">{convert(card.date_event)}</Typography>
+                    <Typography align="left" gutterBottom variant="h4" component="h2">
+                      {card.title}
+                    </Typography>
+                    <Typography variant="h6" align="left">{card.location}</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      size="small"
+                      color="primary"
+                      onClick={() => {
+                        // this.modifyState()
+                        // this.setState(({id: i}))
+                        setShowing(value => !value)
+                        setId({i})
+                      }}
+                    >
+                      See more
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+            {showing ? <DinnerBox state={setShowing(value => !value)} card={events[id]}/> : null}
+        </Grid>
+      </Container>
+    </div>
+  );
 }
 
-  componentDidMount() {
-    axios.get("https://dinnerpool.herokuapp.com/dinners/").then((res) => {
-      this.setState({ events: res.data });
-    });
-  }
-  
-  render() {
-    return (
-      <div>
-        <Container maxWidth="md">
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            <Grid item xs={12} sm={6} md={4}>
-              <Link to="/login">
-                  <Card style={{ height: '100%' }}>
-                    <CardContent>
-                        <AddIcon style={{ color: "green", fontSize:"100px", paddingTop: "15px" }} ></AddIcon>
-                    </CardContent>
-                  </Card>
-                </Link>
-              </Grid>
-            {this.state.events.length > 0 &&
-              // use event id as key instead
-              this.state.events.map((card, i) => (
-                <Grid item key={card.id} xs={12} sm={6} md={4}>
-                  <Card>
-                    <CardContent>
-                      <Typography variant="subtitle2" align="left">{convert(card.date_event)}</Typography>
-                      <Typography align="left" gutterBottom variant="h4" component="h2">
-                        {card.title}
-                      </Typography>
-                      <Typography variant="h6" align="left">{card.location}</Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Button
-                        size="small"
-                        color="primary"
-                        onClick={() => {
-                          this.modifyState()
-                          this.setState(({id: i}))
-                        }}
-                      >
-                        See more
-                      </Button>
-                    </CardActions>
-                  </Card>
-                </Grid>
-              ))}
-              {this.state.isShowing ? <DinnerBox state={this.modifyState} card={this.state.events[this.state.id]}/> : null}
-          </Grid>
-        </Container>
-      </div>
-    );
-  }
-}
+export default DinnerList
+
