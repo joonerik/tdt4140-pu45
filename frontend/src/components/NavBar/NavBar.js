3ff0a8a13329dd61261c8c220368c64eaca78161
Joon-Erik Sæther <joon-erik@hotmail.com>
2021-02-17 16:49:35 +0100

    style: change AppBar with homemade navbar

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -7,15 +7,34 @@ import "./NavBar.css" ;
 
 function NavBar() {
   return (
-    <AppBar position="relative" className="appBar">
-      <Toolbar className="toolBar">
-          <Link className="link" id="menuLink" to="/">MENU</Link>
-          <Link className="link" id="addLink" to="/">ADD</Link>
-          <Link className="link" id="mainLink" to="/">DINNERPOOL</Link>
-          <Link className="link" id="registerLink" to="/login">REGISTER</Link>
-          <Link className="link" id="loginLink" to="/login">LOGIN</Link>
-      </Toolbar>
-    </AppBar>
+    <div class="topnav">
+      <div class="topnav-centered">
+        <Link className="link" id="mainLink" to="/">DINNERPOOL</Link>
+      </div>
+      <Link className="link" id="menuLink" to="/">MENU</Link>
+      <Link className="link" id="addLink" to="/">ADD</Link>
+      
+      <div class="topnav-right">
+        <Link className="link" id="registerLink" to="/login">REGISTER</Link>
+        <Link className="link" id="loginLink" to="/login">LOGIN</Link>
+      </div>
+      
+    </div>
+    // <AppBar className="appBar">
+    //   <Toolbar className="toolBar">
+    //     <div className="leftLinks">
+          // <Link className="link" id="menuLink" to="/">MENU</Link>
+          // <Link className="link" id="addLink" to="/">ADD</Link>
+    //     </div>
+    //     <div className="centerLinks">
+          // <Link className="link" id="mainLink" to="/">DINNERPOOL</Link>
+    //     </div>
+    //     <div className="rightLinks">
+          // <Link className="link" id="registerLink" to="/login">REGISTER</Link>
+          // <Link className="link" id="loginLink" to="/login">LOGIN</Link>
+    //     </div>
+    //   </Toolbar>
+    // </AppBar>
   );
 }
 
