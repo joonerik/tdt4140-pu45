4226ed510f6483eacf1f5f9217a018488430fc0d
Joon-Erik Sæther <joon-erik@hotmail.com>
2021-02-21 21:12:43 +0100

    refactor: remove comments and unused imports etc

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,6 +1,4 @@
 import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
 import { Link } from "react-router-dom";
 import "./NavBar.css" ;
 
@@ -20,21 +18,6 @@ function NavBar() {
       </div>
       
     </div>
-    // <AppBar className="appBar">
-    //   <Toolbar className="toolBar">
-    //     <div className="leftLinks">
-          // <Link className="link" id="menuLink" to="/">MENU</Link>
-          // <Link className="link" id="addLink" to="/">ADD</Link>
-    //     </div>
-    //     <div className="centerLinks">
-          // <Link className="link" id="mainLink" to="/">DINNERPOOL</Link>
-    //     </div>
-    //     <div className="rightLinks">
-          // <Link className="link" id="registerLink" to="/login">REGISTER</Link>
-          // <Link className="link" id="loginLink" to="/login">LOGIN</Link>
-    //     </div>
-    //   </Toolbar>
-    // </AppBar>
   );
 }
 
--- frontend/src/components/__tests__/components/DinnerBox.test.js
+++ frontend/src/components/__tests__/components/DinnerBox.test.js
@@ -2,24 +2,6 @@ import React from 'react';
 import DinnerBox from "../../../components/DinnerBox/DinnerBox"
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
-import ReactDOM from 'react-dom';
-
-// const data = [
-//     {
-//         "title": "Fisk",
-//         "beskrivelse": "Fisk",
-//         "food": "Fisk",
-//         "location": "Norge",
-//         "host": "Ikke midt i en pandemi!",
-//         "capacity": 69
-//     }
-// ]
-
-
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<DinnerBox card={data[0]}></DinnerBox>, div);
-// });
 
 let container = null;
 beforeEach(() => {
