834236d957cd1ee88987f32f69567aaa12f93742
Joon-Erik Sæther <joon-erik@hotmail.com>
2021-02-21 20:56:09 +0100

    test: change DinnerBox test since API is changed

--- frontend/src/components/__tests__/components/DinnerBox.test.js
+++ frontend/src/components/__tests__/components/DinnerBox.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import DinnerBox from "../../../components/DinnerBox/DinnerBox"
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
+import ReactDOM from 'react-dom';
 
 // const data = [
 //     {
@@ -38,12 +39,25 @@ afterEach(() => {
 it('renders dummy data', async () => {
   const data = 
     {
-        "title": "Fisk",
-        "beskrivelse": "Fisk",
-        "food": "Fisk",
-        "location": "Norge",
-        "host": "Ikke midt i en pandemi!",
-        "capacity": 69
+      "id": 1,
+      "title": "Kakestraff",
+      "description": "Beate lager kake til neste gang, justice.",
+      "host": "localhost",
+      "email": "fernando@torres.com",
+      "phone": "98989898",
+      "capacity": 100,
+      "location": "A4",
+      "date_event": "2021-02-21T12:30:00Z",
+      "courses": [
+          "https://dinnerpool.herokuapp.com/courses/3/"
+      ],
+      "price": 0.0,
+      "split_bill": false,
+      "contains_gluten": true,
+      "contains_lactose": true,
+      "contains_nut": true,
+      "contains_shellfish": false,
+      "other_allergens": "meget god stemning"
     };
   jest.spyOn(global, "fetch").mockImplementation(() =>
     Promise.resolve({
@@ -55,7 +69,7 @@ it('renders dummy data', async () => {
     render(<DinnerBox card={data}></DinnerBox>, container)
   })
 
-  expect(container.querySelector('h1').textContent).toBe(data.title.toUpperCase())
+  expect(container.querySelector('h1').textContent).toBe(data.title)
 
   global.fetch.mockRestore();
 
