35bae96212a66dbc2d94cfcc9b9c99af8da38e3b
Joon-Erik Sæther <joon-erik@hotmail.com>
2021-03-11 21:21:27 +0100

    feat: add ContextProvider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import DinnerOverview from "./pages/DinnerOverview";
 import LoginPage from "./pages/LoginPage";
@@ -6,23 +7,35 @@ import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer"
 import DinnerForm from "./pages/DinnerForm";
 import RegisterPage from "./pages/RegisterPage";
+import { AuthContext } from './components/UserContext/auth'
+import PrivateRoute from './PrivateRoute'
 
-function App() {
+function App(props) {
+
+  const [authTokens, setAuthTokens] = useState();
+
+  const setTokens = (data) => {
+    localStorage.setItem('tokens', JSON.stringify(data));
+    setAuthTokens(data)
+  }
+
   return (
-    <div className="App">
-        <Router>
-          <NavBar />
-          <Switch>
-            <Route component={DinnerOverview} exact path="/" />
-            <Route component={LoginPage} path="/login" />
-            <Route component={DinnerForm} path="/add" />
-            <Route component={RegisterPage} path="/register" />
-            <Route data-testid="elseLink" component={NoMatch} />
-          </Switch>
-          <Footer/>
-        </Router>
-
-    </div>
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+      <div className="App">
+          <Router>
+            <NavBar />
+            <Switch>
+              <Route component={DinnerOverview} exact path="/" />
+              <Route component={LoginPage} path="/login" />
+              {/* <Route component={DinnerForm} path="/add" /> */}
+              <Route component={RegisterPage} path="/register" />
+              <PrivateRoute path="/add" component={DinnerForm} />
+              <Route data-testid="elseLink" component={NoMatch} />
+            </Switch>
+            <Footer/>
+          </Router>
+      </div>
+    </AuthContext.Provider>
   );
 }
 
